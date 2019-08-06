1dca38ed0bea7f71c34d40974b3f519c86b49e6b
Fraser Provan <FraserProvan2@gmail.com>
2019-08-06 21:11:55 +0100

    feed tests

--- tests/Feature/FeedTest.php
+++ tests/Feature/FeedTest.php
@@ -2,45 +2,65 @@
 
 namespace Tests\Feature;
 
-use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 
 class FeedTest extends TestCase
 {
     use RefreshDatabase;
 
     /** @test */
-    public function home_feed_loads()
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
-
-    /** @test */
     public function all_feed_loads()
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+        $this->get('/all')
+            ->assertOk()
+            ->assertSee('/All');
     }
 
     /** @test */
-    public function get_can_access_home()
+    public function home_feed_loads()
     {
+        $this->signIn();
 
+        $this->get('/')
+            ->assertOk()
+            ->assertSee('/Home');
     }
 
     /** @test */
-    public function feed_orders_by_votes()
+    public function guest_cant_access_home()
     {
-        //
+        $this->get('/')
+            ->assertRedirect('/all');
     }
 
-    public function creating_feed_is_as_expected()
+    /** @test */
+    public function feed_default_order_by_is_votes()
     {
-        //
+        $this->seedDb();
+
+        $response = $this->get('/all');
+
+        $posts = $this->getViewsData($response, 'posts');
+
+        $this->assertTrue($posts[0]->votes > $posts[1]->votes);
+        $this->assertTrue($posts[1]->votes > $posts[2]->votes);
+        $this->assertTrue($posts[2]->votes > $posts[3]->votes);
+        $this->assertTrue($posts[3]->votes > $posts[4]->votes);
+        $this->assertTrue($posts[4]->votes > $posts[5]->votes);
     }
 
+    /** @test */
+    public function pagination_works_as_expected()
+    {
+        $this->seedDb();
+        $this->signIn();
+
+        $this->get('/')
+            ->assertSeeTextInOrder(['Previous', 'Next']);
+
+        $this->get('/all?page=2')
+            ->assertViewHas('posts')
+            ->assertSee('post-bg');
+    }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,41 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Acts as a user for rests
+     *
+     * @return InteractsWithAuthentication user
+     */
+    public function signIn($user = null)
+    {
+        // If user, else create
+        return $this->actingAs($user ?: factory('App\User')->create())
+            ->assertAuthenticated();
+    }
+
+    /**
+     * Creates mock data when called
+     *
+     * @return InteractsWithDatabase seeds DB
+     */
+    public function seedDb()
+    {
+        return $this->seed('SubRedditSeeder');
+    }
+
+    /**
+     * Gets data as its passed into a view, specify
+     * by the variable ($key)
+     *
+     * @return InteractsWithDatabase seeds DB
+     */
+    protected function getViewsData($test_response, $key)
+    {
+        $content = $test_response->getOriginalContent();
+
+        $content = $content->getData();
+
+        return $content[$key]->all();
+    }
 }
